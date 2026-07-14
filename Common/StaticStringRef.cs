332b1b258d5fda398837fdcd2be7087f5465dd6c
Phoenix-schm <ellaschmitt2016@gmail.com>
2026-07-14 12:33:27 -0500

    Refactored DragIngredientManager alongside Cooker and IngredientPackage to utilize Groups for Interface-like function calls
    - Removed hoveredCooker and hoveredPackage. Now use single Node hoveredStorage.
    - IngredientPackage and Cooker contain their ingredient logic more.

--- Common/StaticStringRef.cs
+++ Common/StaticStringRef.cs
@@ -12,6 +12,11 @@ public static class StaticStringRef
     public static readonly StringName position = "position";
     public static readonly StringName scale = "scale";
 
-    public static readonly StringName primaryInteraction = "Primary Interaction Button";
-    public static readonly StringName secondaryInteraction = "Secondary Interaction Button";
+    public static readonly StringName a_primaryInteraction = "Primary Interaction Button";
+    public static readonly StringName a_secondaryInteraction = "Secondary Interaction Button";
+
+    public static readonly StringName G_IngredientStorage = "Ingredient Storage";
+    public static readonly StringName f_TakeIngredient = "TakeIngredient";
+    public static readonly StringName f_TryPlaceIngredient = "TryPlaceIngredient";
+    public static readonly StringName f_TryReturnIngredient = "TryReturnIngredientToParent";
 }
